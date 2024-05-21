72c3f106f3800709f219611375f247f7e4e35659
adrisala20 <adriana_15_tuya@hotmail.com>
2024-05-21 17:06:37 +0200

    linked db for movies

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'movies')
+
+@section('content')
+<section>
+    <h1>movies</h1>
+</section>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MovieController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::get('/', [HomeController::class,'index'])->name('home');
+Route::get('/movies', [MovieController::class,'index'])->name('movies.index');
+
+
